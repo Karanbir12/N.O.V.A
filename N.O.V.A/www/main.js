25f5e4de525444583ad33ffbdf5fb535cced7866
Karanbir Singh <150276845+Karanbir12@users.noreply.github.com>
2025-07-18 00:19:47 +0530

    Add files via upload

---
+++ N.O.V.A/www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.txt').textillate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"bounceIn",
+        },
+         out:{
+            effect:"bounceOut",
+        },
+    })
+});
